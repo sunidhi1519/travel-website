7f7bee207e597c497d0c6b44de602d8cdcf8cac4
Sunidhi1519 <sunidhi190103@gmail.com>
2023-06-16 23:41:28 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
-const header = document.querySelector("header");
+document.addEventListener("DOMContentLoaded", function () {
 
-window.addEventListener("scroll", function() {
-    header.classList.toggle("sticky",window.scrollY>0)
+    let menu = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+    };
+
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('open');
+    };
+
+    let videoWrapper = document.querySelector('.video-wrapper');
+    let video = videoWrapper.querySelector('video');
+    let playBtn = videoWrapper.querySelector('#play-btn');
+
+    playBtn.addEventListener('click', () => {
+        video.play();
+        playBtn.style.display = 'none'; // Hide the play button
+    });
+
+    video.addEventListener('click', () => {
+        if (video.paused) {
+            video.play();
+            playBtn.style.display = 'none'; // Hide the play button
+        } else {
+            video.pause();
+        }
+    });
+
+    video.addEventListener('ended', () => {
+        playBtn.style.display = 'block'; // Make the play button visible again
+    });
+
+
+
 });
-
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle(open);
-
-};
-
-
-window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('open');
-};
